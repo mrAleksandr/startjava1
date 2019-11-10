21f37fd79876d847bb4bd933aa3675cff861528b
mrAleksandr <shirokov.a.u@gmail.com>
2019-11-10 23:32:07 +0300

    implement HW_1

---
+++ Variable.java
@@ -0,0 +1,21 @@
+public class Variable{
+	public static void main(String[] args) {
+		byte architecture = 64;
+		short core_quantity = 5;
+		int	processor_value = 3450;
+		long license_number = 1234567890;
+		float ghz = 3.10f;
+		double ram = 14.0d;
+		char main_disc = 'C';
+		boolean activation = true;
+		System.out.println("byte "+"architecture: "+architecture);
+		System.out.println("short "+"core_quantity: "+core_quantity);
+		System.out.println("int "+"processor_value: "+processor_value);
+		System.out.println("long "+"license_number: "+license_number);
+		System.out.println("float "+"ghz: "+ghz);
+		System.out.println("double "+"ram: "+ram);
+		System.out.println("char "+"main_disc: "+main_disc);
+		System.out.println("boolean "+"activation: "+activation);
+	}
+
+}
